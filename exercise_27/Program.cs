08702734c18c50d07cf2df33a4e2580cea95707f
will <wschoenle93@gmail.com>
2021-04-21 18:25:10 -0400

    first commit for exercise 27

---
+++ exercise_27/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice_3
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your submission:");
+            string userInput = Console.ReadLine().ToLower();
+            Console.WriteLine(VowelCount(userInput));
+
+        }
+
+        public static int VowelCount(string input)
+        {
+            int total = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] != vowels[0]) || (input[i] != vowels[1]) || (input[i] != vowels[2]) ||
+                    (input[i] != vowels[3]) || (input[i] != vowels[4]))
+
+                {
+                    total = total + 1;
+                }
+            }
+            return total;
+
+        }
+    }
+}
